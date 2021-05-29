ea86d10dcbafc754787154f98ed1d21bac5565d3
Shivam Sharma <the.alpha.tronixm@gmail.com>
2021-05-30 02:24:49 +0530

    added-navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.js'
+import Home from './components/home'
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       Portfolio App
     </div>
   );
