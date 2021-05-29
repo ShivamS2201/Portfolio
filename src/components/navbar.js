58fa97096b655ee9f9a46d20550a5225ad548547
Shivam Sharma <the.alpha.tronixm@gmail.com>
2021-05-30 01:42:45 +0530

    Portfolio Project

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+ 
+function Navbar(){
+    return(
+        <div class="Nav-container">
+            
+  <a href="#">ABOUT</a>
+  <a href="#">EXPERIENCE</a>
+  <a href="#">PROJECTS</a>
+  <a href="#" >EXTRACURRICULAR</a>
+</div>
+        
+    )
+}
+
+export default Navbar;
