8ecc949f4aa598ee9c26c620408922ea51bd0b80
Shivam Sharma <the.alpha.tronixm@gmail.com>
2021-06-01 14:46:39 +0530

    added projs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.js'
-import Home from './components/home'
+import Home from './components/home';
+import Project from './components/project'
 function App() {
   return (
     <div className="App">
       
       <Navbar />
       <Home />
+      <Project />
     </div>
   );
 }
